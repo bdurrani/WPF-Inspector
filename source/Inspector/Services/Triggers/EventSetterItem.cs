290f82663347ced1bb1913008f42cfd4426063c5
Bilal Durrani <bdurrani@BDUROPT9020.ni.corp.natinst.com>
2016-02-10 11:38:46 -0500

    Fixed for EventSetter crash that came up while testing my app
    Import WPFInspector from codeplex Feb 10, 2016

---
+++ source/Inspector/Services/Triggers/EventSetterItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace ChristianMoser.WpfInspector.Services.Triggers
+{
+    /// <summary>
+    /// Abstraction model of a <see cref="EventSetter"/>
+    /// </summary>
+    public class EventSetterItem
+    {
+        public EventSetterItem(string eventName, string handler)
+        {
+            EventName = eventName;
+            Handler = handler;
+        }
+
+        public string EventName { get; private set; }
+        public string Handler { get; private set; }
+    }
+}
